abb27d87e249cbaaf497e67738ab6d8df86722a1
viaDragonz <viavalve@gmail.com>
2018-06-29 23:14:36 -0400

    Added Discord Rules Quiz
    Rules Quiz Beta v1.0 added.
    -Banner added to top
    -Command added
    -Check mark image added
    -Questions added
    -login.html removed
    -landing.html changed

---
+++ discord/assets/js/quiz.js
@@ -0,0 +1,37 @@
+function findButton(){
+	const button = document.getElementById("button");
+	button.addEventListener("click", doDisplay, false);
+}
+
+function checkForm() {
+    var one = document.getElementById("spam").value;
+    var two = document.getElementById("ddos").value;
+    var three = document.getElementById("say").value;
+    var four = document.getElementById("agree").value;
+    console.log("one " + one);
+    console.log("2 " + two);
+    console.log("3 " + three);
+    console.log("4 " + four);
+    if (one === "#spam" && two === "s3" && three === "staff" && four === "Agree") {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+function doDisplay() {
+    let form = checkForm();
+    if (form !== true) {
+        alert("You did not answer the quiz correctly!");
+    } else {
+        let commandBox = document.getElementById("commandbox");
+        let icon = document.getElementById("icons");
+        commandBox.innerHTML = '<h1>Congrats on passing the rules quiz!<h1> <br> <h2>To obtain the Confirmed role, type "/asqw234f" in #confirm<h2><br><h3>If the command does not work, DM a @moderator with the command.<br>'
+        icon.innerHTML = '<img src="checkmark.png" style="height:50px; width:50px;" />'
+    };
+}
+
+
+
+
+window.addEventListener("load", findButton, false);
